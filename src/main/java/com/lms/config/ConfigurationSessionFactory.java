9fc15f94e135c405d4637a6a0e6c66d429b91f13
Yana Ilieva <yanailieva199@gmail.com>
2020-10-22 09:44:15 +0300

    Small fix on configs

--- src/main/java/com/lms/config/ConfigurationSessionFactory.java
+++ src/main/java/com/lms/config/ConfigurationSessionFactory.java
@@ -1,10 +1,7 @@
 package com.lms.config;
 
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 
 public class ConfigurationSessionFactory {
 
