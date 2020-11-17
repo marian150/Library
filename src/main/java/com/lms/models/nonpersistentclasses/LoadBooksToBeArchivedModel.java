74109ca67d9b1b150e72a10a3c61907e3f4d53e7
Yana Ilieva <yanailieva199@gmail.com>
2020-11-17 21:18:37 +0200

    Notification for books to be archived finished

---
+++ src/main/java/com/lms/models/nonpersistentclasses/LoadBooksToBeArchivedModel.java
@@ -0,0 +1,49 @@
+package com.lms.models.nonpersistentclasses;
+
+public class BooksToBeArchivedModel {
+    private String inv;
+    private String title;
+    private String author;
+    private String year;
+    private String isbn;
+
+    public String getInv() {
+        return inv;
+    }
+
+    public void setInv(String inv) {
+        this.inv = inv;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
